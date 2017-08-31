877e748a7ea74a90cbc00bf6604b8b2c0bec6ad0
Konstantin Kutsy <kutsykon@gmail.com>
2017-08-31 18:31:45 +0300

    fetch gmaps geocods

--- handlers.py
+++ handlers.py
@@ -1,5 +1,15 @@
 
+import json
+from tornado import gen
 from tornado.web import RequestHandler
+from tornado.httpclient import AsyncHTTPClient
+
+gmaps_key = ''
+wiki_key = ''
+
+async def fetch_json(url):
+    response = await AsyncHTTPClient().fetch(url)
+    return json.loads(response.body)
 
 
 class BaseHandler(RequestHandler):
@@ -9,8 +19,12 @@ class BaseHandler(RequestHandler):
 
 class GeoHandler(BaseHandler):
 
-    def get(self, *args, **kwargs):
-        pass
+    async def get(self):
+        addr = self.get_argument('address')
+        gmaps_qry = f'https://maps.googleapis.com/maps/api/geocode/json?' \
+                    f'address={addr}&key={gmaps_key}'
+        res = await fetch_json(gmaps_qry)
+        self.write(res.get('results')[0]['geometry']['location'])
 
 
 class WikiHandler(BaseHandler):
